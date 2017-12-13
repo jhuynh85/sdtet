a2bffc349f23b1f12917940d17349c140523da5a
alanttran <mr.alan.tran@gmail.com>
2017-12-12 21:31:11 -0800

    finish content for schedule

--- src/components/entertainment/entertainment.vue
+++ src/components/entertainment/entertainment.vue
@@ -3,42 +3,160 @@
 		<h1>Event Schedule</h1>
 		<div layout="row" layout-align="center">
 			<div class="sdtet-schedule-grid">
-			  <div class="sdtet-schedule-title">Main Stage</div>
-			  <div class="sdtet-schedule-date">FRIDAY 02.16.18</div>
-			  <div class="sdtet-schedule-title">Cultural Village</div>
-			  <div class="sdtet-schedule-divider"></div>
 
-			  <div class="sdtet-schedule-main-stage">Festival Opens - Lion Dance</div>
-			  <div class="sdtet-schedule-date">5:00 PM</div>
-			  <div class="sdtet-schedule-cultural-village">Festival Opens</div>
+			<!-- Friday -->
+			  	<div class="sdtet-schedule-title">Main Stage</div>
+			  	<div class="sdtet-schedule-date">FRIDAY 02.16.18</div>
+			  	<div class="sdtet-schedule-title">Cultural Village</div>
+			  	<div class="sdtet-schedule-divider"></div>
 
-			  <div class="sdtet-schedule-main-stage">Tammy Pham</div>
-			  <div class="sdtet-schedule-date">5:30 PM</div>
-			  <div class="sdtet-schedule-cultural-village">Legendary Lion Dance</div>
+			  	<div class="sdtet-schedule-main-stage">Festival Opens - Lion Dance</div>
+			  	<div class="sdtet-schedule-date">5:00 PM</div>
+			  	<div class="sdtet-schedule-cultural-village">Festival Opens</div>
 
-			  <div class="sdtet-schedule-main-stage">Pageant Fashion Show</div>
-			  <div class="sdtet-schedule-date">6:30 PM</div>
-			  <div class="sdtet-schedule-cultural-village"></div>
+			  	<div class="sdtet-schedule-main-stage">Tammy Pham</div>
+			  	<div class="sdtet-schedule-date">5:30 PM</div>
+			  	<div class="sdtet-schedule-cultural-village">Legendary Lion Dance</div>
 
-			  <div class="sdtet-schedule-main-stage">B-Boy Battle Break Dance Contest</div>
-			  <div class="sdtet-schedule-date">7:00 PM</div>
-			  <div class="sdtet-schedule-cultural-village">Traditional Singers</div>
+			  	<div class="sdtet-schedule-main-stage">Pageant Fashion Show</div>
+			  	<div class="sdtet-schedule-date">6:30 PM</div>
+			  	<div class="sdtet-schedule-cultural-village"></div>
 
-			  <div class="sdtet-schedule-main-stage">Hoang Thanh</div>
-			  <div class="sdtet-schedule-date">8:00 PM</div>
-			  <div class="sdtet-schedule-cultural-village">Festival Opens</div>
+			  	<div class="sdtet-schedule-main-stage">B-Boy Battle Break Dance Contest</div>
+			  	<div class="sdtet-schedule-date">7:00 PM</div>
+			  	<div class="sdtet-schedule-cultural-village">Traditional Singers</div>
 
-			  <div class="sdtet-schedule-main-stage">Step Up Dance</div>
-			  <div class="sdtet-schedule-date">8:30 PM</div>
-			  <div class="sdtet-schedule-cultural-village"></div>
+			  	<div class="sdtet-schedule-main-stage">Hoang Thanh</div>
+			  	<div class="sdtet-schedule-date">8:00 PM</div>
+			  	<div class="sdtet-schedule-cultural-village">Festival Opens</div>
 
-			  <div class="sdtet-schedule-main-stage">Step Up Dance winners</div>
-			  <div class="sdtet-schedule-date">9:30 PM</div>
-			  <div class="sdtet-schedule-cultural-village"></div>
+			  	<div class="sdtet-schedule-main-stage">Step Up Dance</div>
+			  	<div class="sdtet-schedule-date">8:30 PM</div>
+			  	<div class="sdtet-schedule-cultural-village"></div>
 
-			  <div class="sdtet-schedule-main-stage">Festival Closes</div>
-			  <div class="sdtet-schedule-date">10:00 PM</div>
-			  <div class="sdtet-schedule-cultural-village">Festival Closes</div>
+			  	<div class="sdtet-schedule-main-stage">Step Up Dance winners</div>
+			  	<div class="sdtet-schedule-date">9:30 PM</div>
+			  	<div class="sdtet-schedule-cultural-village"></div>
+
+			  	<div class="sdtet-schedule-main-stage">Festival Closes</div>
+			  	<div class="sdtet-schedule-date">10:00 PM</div>
+			  	<div class="sdtet-schedule-cultural-village">Festival Closes</div>
+
+			<!-- Saturday -->
+			  	<div class="sdtet-schedule-title">Main Stage</div>
+			  	<div class="sdtet-schedule-date">SATURDAY 02.17.18</div>
+			  	<div class="sdtet-schedule-title">Cultural Village</div>
+			  	<div class="sdtet-schedule-divider"></div>
+
+			  	<div class="sdtet-schedule-main-stage">Festival Opens - Lion Dance</div>
+			  	<div class="sdtet-schedule-date">11:00 AM</div>
+			  	<div class="sdtet-schedule-cultural-village">Festival Opens</div>
+
+			  	<div class="sdtet-schedule-main-stage">Opening Drift</div>
+			  	<div class="sdtet-schedule-date">12:00 PM</div>
+			  	<div class="sdtet-schedule-cultural-village">Legendary Lion Dance</div>
+
+			  	<div class="sdtet-schedule-main-stage">Hoa Nghiem Lion Dance</div>
+			  	<div class="sdtet-schedule-date">1:00 PM</div>
+			  	<div class="sdtet-schedule-cultural-village">Royal Procession</div>
+
+			  	<div class="sdtet-schedule-main-stage"></div>
+			  	<div class="sdtet-schedule-date">1:30 PM</div>
+			  	<div class="sdtet-schedule-cultural-village">Poetic Inspiration</div>
+
+			  	<div class="sdtet-schedule-main-stage"></div>
+			  	<div class="sdtet-schedule-date">3:00 PM</div>
+			  	<div class="sdtet-schedule-cultural-village">Rice Drum Dance</div>
+
+			  	<div class="sdtet-schedule-main-stage"></div>
+			  	<div class="sdtet-schedule-date">3:30 PM</div>
+			  	<div class="sdtet-schedule-cultural-village">Poetic Inspiration</div>
+
+			  	<div class="sdtet-schedule-main-stage">Hiep Hoi Culture Show</div>
+			  	<div class="sdtet-schedule-date">4:00 PM</div>
+			  	<div class="sdtet-schedule-cultural-village"></div>
+
+			  	<div class="sdtet-schedule-main-stage">Legendary Lion Dance</div>
+			  	<div class="sdtet-schedule-date">5:00 PM</div>
+			  	<div class="sdtet-schedule-cultural-village"></div>
+
+			  	<div class="sdtet-schedule-main-stage">Miss Vietnam Pageant</div>
+			  	<div class="sdtet-schedule-date">5:30 PM</div>
+			  	<div class="sdtet-schedule-cultural-village"></div>
+
+			  	<div class="sdtet-schedule-main-stage"></div>
+			  	<div class="sdtet-schedule-date">6:30 PM</div>
+			  	<div class="sdtet-schedule-cultural-village">Traditional Singers</div>
+
+			  	<div class="sdtet-schedule-main-stage">Ho Hoang Yen</div>
+			  	<div class="sdtet-schedule-date">7:30 PM</div>
+			  	<div class="sdtet-schedule-cultural-village"></div>
+
+			  	<div class="sdtet-schedule-main-stage">Miss Vietnam Pageant Winners</div>
+			  	<div class="sdtet-schedule-date">9:30 PM</div>
+			  	<div class="sdtet-schedule-cultural-village"></div>
+
+			  	<div class="sdtet-schedule-main-stage">Festival Closes</div>
+			  	<div class="sdtet-schedule-date">10:00 PM</div>
+			  	<div class="sdtet-schedule-cultural-village">Festival Closes</div>
+
+			 <!-- Sunday -->
+			  	<div class="sdtet-schedule-title">Main Stage</div>
+			  	<div class="sdtet-schedule-date">SUNDAY 02.18.18</div>
+			  	<div class="sdtet-schedule-title">Cultural Village</div>
+			  	<div class="sdtet-schedule-divider"></div>
+
+			  	<div class="sdtet-schedule-main-stage">Festival Opens - Lion Dance</div>
+			  	<div class="sdtet-schedule-date">11:00 AM</div>
+			  	<div class="sdtet-schedule-cultural-village">Festival Opens</div>
+
+			  	<div class="sdtet-schedule-main-stage">Opening Drift</div>
+			  	<div class="sdtet-schedule-date">12:00 PM</div>
+			  	<div class="sdtet-schedule-cultural-village">Legendary Lion Dance</div>
+
+			  	<div class="sdtet-schedule-main-stage">Hoa Nghiem Lion Dance</div>
+			  	<div class="sdtet-schedule-date">1:00 PM</div>
+			  	<div class="sdtet-schedule-cultural-village">Royal Procession</div>
+
+			  	<div class="sdtet-schedule-main-stage"></div>
+			  	<div class="sdtet-schedule-date">1:30 PM</div>
+			  	<div class="sdtet-schedule-cultural-village">Poetic Inspiration</div>
+
+			  	<div class="sdtet-schedule-main-stage"></div>
+			  	<div class="sdtet-schedule-date">3:00 PM</div>
+			  	<div class="sdtet-schedule-cultural-village">Rice Drum Dance</div>
+
+			  	<div class="sdtet-schedule-main-stage"></div>
+			  	<div class="sdtet-schedule-date">3:30 PM</div>
+			  	<div class="sdtet-schedule-cultural-village">Poetic Inspiration</div>
+
+			  	<div class="sdtet-schedule-main-stage">Hiep Hoi Culture Show</div>
+			  	<div class="sdtet-schedule-date">4:00 PM</div>
+			  	<div class="sdtet-schedule-cultural-village"></div>
+
+			  	<div class="sdtet-schedule-main-stage">Legendary Lion Dance</div>
+			  	<div class="sdtet-schedule-date">5:00 PM</div>
+			  	<div class="sdtet-schedule-cultural-village"></div>
+
+			  	<div class="sdtet-schedule-main-stage">Miss Vietnam Pageant</div>
+			  	<div class="sdtet-schedule-date">5:30 PM</div>
+			  	<div class="sdtet-schedule-cultural-village"></div>
+
+			  	<div class="sdtet-schedule-main-stage"></div>
+			  	<div class="sdtet-schedule-date">6:30 PM</div>
+			  	<div class="sdtet-schedule-cultural-village">Traditional Singers</div>
+
+			  	<div class="sdtet-schedule-main-stage">Ho Hoang Yen</div>
+			  	<div class="sdtet-schedule-date">7:30 PM</div>
+			  	<div class="sdtet-schedule-cultural-village"></div>
+
+			  	<div class="sdtet-schedule-main-stage">Miss Vietnam Pageant Winners</div>
+			  	<div class="sdtet-schedule-date">9:30 PM</div>
+			  	<div class="sdtet-schedule-cultural-village"></div>
+
+			  	<div class="sdtet-schedule-main-stage">Festival Closes</div>
+			  	<div class="sdtet-schedule-date">10:00 PM</div>
+			  	<div class="sdtet-schedule-cultural-village">Festival Closes</div>
 			</div>
 		</div>
 	</div>
