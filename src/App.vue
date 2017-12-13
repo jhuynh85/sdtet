abcf14a748a46c2af16c867cd98d1f7fa1de1928
alanttran <mr.alan.tran@gmail.com>
2017-12-12 20:33:47 -0800

    added components

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- <img src="./assets/logo.png"> -->
+    <navigation></navigation>
+    <entertainment></entertainment>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navigation from './components/navigation/navigation'
+import Entertainment from './components/entertainment/entertainment'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    Navigation,
+    Entertainment
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/navigation/navigation.vue
@@ -0,0 +1,27 @@
+<template>
+  <div layout="row" layout-align="space-between center" class="sdtet-navigation sdtet-container">
+    <div flex-none>
+      <img src="http://placehold.it/200x100" alt="Logo">
+    </div>
+    <div class="sdtet-navigation__links" flex-auto layout="row" layout-align="space-around">
+      <div flex-none><a href="">Schedule</a></div>
+      <div flex-none><a href="">Pageant</a></div>
+      <div flex-none><a href="">Sponsors</a></div>
+      <div flex-none><a href="">Vendors</a></div>
+      <div flex-none><a href="">Volunteers</a></div>
+      <div flex-none><a href="">Contact</a></div>
+      <div flex-none><a href="">Donate</a></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'navigation',
+    data () {
+      return {
+        title: 'Navigation!'
+      }
+    }
+  }
+</script>
