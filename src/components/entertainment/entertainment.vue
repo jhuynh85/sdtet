af0ad3b004b8ddbbb2b5c3be573ac1f4c6308258
alanttran <mr.alan.tran@gmail.com>
2018-01-03 19:19:04 -0800

    restructured sass files so watcher works better

--- src/components/entertainment/entertainment.vue
+++ src/components/entertainment/entertainment.vue
@@ -2,6 +2,58 @@
     <div class="sdtet-background-red sdtet-entertainment-container">
         <div class="sdtet-container">
             <h1 class="sdtet-text-align-center">Entertainment</h1>
+            <p class="sdtet-text-align-center">Would you like to be part of our entertainment program? Apply below!</p>
+            <v-layout>
+                <v-flex>
+                    <v-card class="sdtet-entertainment-card">
+                        <v-card-media src="../../../static/images/StepUpDance.jpg" height="200px">
+                        </v-card-media>
+                        <v-card-title primary-title>
+                          <div>
+                            <h3 class="headline mb-0">Step Up Dance Competition</h3>
+                            <div>Do you love dancing? If you have an up and coming team or a new choreography to try, why not bring yourselves to our state and wow us for the new year!</div>
+                          </div>
+                        </v-card-title>
+                        <v-card-actions>
+                          <v-btn flat color="orange">Share</v-btn>
+                          <v-btn flat color="orange">Explore</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-flex>
+                <v-flex>
+                    <v-card class="sdtet-entertainment-card">
+                        <v-card-media src="../../../static/images/StepUpDance.jpg" height="200px">
+                        </v-card-media>
+                        <v-card-title primary-title>
+                          <div>
+                            <h3 class="headline mb-0">Step Up Dance Competition</h3>
+                            <div>Do you love dancing? If you have an up and coming team or a new choreography to try, why not bring yourselves to our state and wow us for the new year!</div>
+                          </div>
+                        </v-card-title>
+                        <v-card-actions>
+                          <v-btn flat color="orange">Share</v-btn>
+                          <v-btn flat color="orange">Explore</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-flex>
+                <v-flex>
+                    <v-card class="sdtet-entertainment-card">
+                        <v-card-media src="../../../static/images/StepUpDance.jpg" height="200px">
+                        </v-card-media>
+                        <v-card-title primary-title>
+                          <div>
+                            <h3 class="headline mb-0">Step Up Dance Competition</h3>
+                            <div>Do you love dancing? If you have an up and coming team or a new choreography to try, why not bring yourselves to our state and wow us for the new year!</div>
+                          </div>
+                        </v-card-title>
+                        <v-card-actions>
+                          <v-btn flat color="orange">Share</v-btn>
+                          <v-btn flat color="orange">Explore</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+            
             <div class="team-member">
                 <a href="#" target="_blank"><img width="128" height="128" src="http://placehold.it/128x128" class="img-responsive img-circle" alt=""></a>
                 <h4>Step Up Dance Competition</h4>
