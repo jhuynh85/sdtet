2ecc8b23cf434ac7c8374258c3fc33eb57fb42c9
Joseph Huynh <joe.huynh@gmail.com>
2018-01-08 14:49:05 -0800

    Added hamburger menu for navigation links

--- src/components/navigation/navigation.vue
+++ src/components/navigation/navigation.vue
@@ -12,6 +12,36 @@
       <div flex-none><a href="#sdtet-contact-section" v-smooth-scroll>Contact</a></div>
       <div flex-none><a href="https://www.paypal.com/fundraiser/charity/1440775" target="_blank">Donate</a></div>
     </div>
+    <div class="sdtet-navigation-menu">
+      <v-menu offset-y>
+      <v-btn icon large slot="activator">
+        <v-icon large color="red">menu</v-icon>
+      </v-btn>
+      <v-list>
+              <v-list-tile>
+                <a href="#sdtet-schedule-section" v-smooth-scroll><v-list-tile-title>Schedule</v-list-tile-title></a>
+              </v-list-tile>
+              <v-list-tile>
+                <a href="#sdtet-pageant-section" v-smooth-scroll><v-list-tile-title>Pageant</v-list-tile-title></a>
+              </v-list-tile>
+              <v-list-tile>
+                <a href="#sdtet-sponsors-section" v-smooth-scroll><v-list-tile-title>Sponsors</v-list-tile-title></a>
+              </v-list-tile>
+              <v-list-tile>
+                <a href="#sdtet-vendors-section" v-smooth-scroll><v-list-tile-title>Vendors</v-list-tile-title></a>
+              </v-list-tile>
+              <v-list-tile>
+                <a href="#sdtet-volunteers-section" v-smooth-scroll><v-list-tile-title>Volunteers</v-list-tile-title></a>
+              </v-list-tile>
+              <v-list-tile>
+                <a href="#sdtet-contact-section" v-smooth-scroll><v-list-tile-title>Contact</v-list-tile-title></a>
+              </v-list-tile>
+              <v-list-tile>
+                <a href="https://www.paypal.com/fundraiser/charity/1440775"><v-list-tile-title>Donate</v-list-tile-title></a>
+              </v-list-tile>
+            </v-list>
+      </v-menu>
+    </div>
   </div>
 </template>
 
