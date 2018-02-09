000c8cfb83fa1c7dde4353e5bb31f9f7c0e70f2e
alanttran <mr.alan.tran@gmail.com>
2018-02-09 00:12:03 -0800

    hooked up schedule to api

--- src/components/schedule/schedule.vue
+++ src/components/schedule/schedule.vue
@@ -1,178 +1,94 @@
 <template>
 <div class="sdtet-schedule-background">
-	<div id="sdtet-schedule-section" class="sdtet-schedule-container sdtet-container">
-		<h1>Event Schedule</h1>
-		<p>Events and times can change without notice</p>
-		<div layout="row" layout-align="center">
-			<div flex="none" layout="row" class="sdtet-schedule-grid">
+    <div id="sdtet-schedule-section" class="sdtet-schedule-container sdtet-container">
+        <h1>Event Schedule</h1>
+        <p>Events and times can change without notice</p>
+        <div layout="row" layout-align="center">
+            <div flex="none" layout="row" class="sdtet-schedule-grid">
 
-			<!-- Friday -->
+            <!-- Friday -->
 
-			  	<div flex="40" class="sdtet-schedule-title sdtet-text-align-right ">Main Stage</div>
-			  	<div flex="20" class="sdtet-schedule-date">FRIDAY 02.16.18</div>
-			  	<div flex="40" class="sdtet-schedule-title sdtet-text-align-left ">Cultural Village</div>
-			  	<div class="sdtet-schedule-divider"></div>
+                <div flex="40" class="sdtet-schedule-title sdtet-text-align-right ">Main Stage</div>
+                <div flex="20" class="sdtet-schedule-date">FRIDAY 02.16.18</div>
+                <div flex="40" class="sdtet-schedule-title sdtet-text-align-left ">Cultural Village</div>
+                <div class="sdtet-schedule-divider"></div>
+                
+                <div v-for="event in friday" :key="event.id" layout="row" flex="100">
+                    <div flex="40" class="sdtet-schedule-main-stage pa-1">{{event.english_event}}</div>
+                    <div flex="20" class="sdtet-schedule-date">{{event.start_time}}</div>
+                    <div flex="40" class="sdtet-schedule-cultural-village">{{event.viet_event}}</div>
+                </div>
+                
+                <!-- Saturday -->
+                <div class="sdtet-schedule-spacer"></div>
+                <div flex="40" class="sdtet-schedule-title sdtet-text-align-right">Main Stage</div>
+                <div flex="20" class="sdtet-schedule-date">SATURDAY 02.17.18</div>
+                <div flex="40" class="sdtet-schedule-title sdtet-text-align-left">Cultural Village</div>
+                <div class="sdtet-schedule-divider"></div>
 
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Festival Opens - Legendary Lion Dance</div>
-			  	<div flex="20" class="sdtet-schedule-date">5:00 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village">Festival Opens</div>
+                <div v-for="event in saturday" :key="event.id" layout="row" flex="100">
+                    <div flex="40" class="sdtet-schedule-main-stage pa-1">{{event.english_event}}</div>
+                    <div flex="20" class="sdtet-schedule-date">{{event.start_time}}</div>
+                    <div flex="40" class="sdtet-schedule-cultural-village">{{event.viet_event}}</div>
+                </div>
 
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Jenya Carter</div>
-			  	<div flex="20" class="sdtet-schedule-date">5:30 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village">Tet Music</div>
+                <!-- Sunday -->
+                <div class="sdtet-schedule-spacer"></div>
+                <div flex="40" class="sdtet-schedule-title sdtet-text-align-right">Main Stage</div>
+                <div flex="20" class="sdtet-schedule-date">SUNDAY 02.18.18</div>
+                <div flex="40" class="sdtet-schedule-title sdtet-text-align-left">Cultural Village</div>
+                <div class="sdtet-schedule-divider"></div>
 
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Spoken-Word Artist Mr. Viet Mai</div>
-			  	<div flex="20" class="sdtet-schedule-date">5:45 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village"></div>
+                <div v-for="event in sunday" :key="event.id" layout="row" flex="100">
+                    <div flex="40" class="sdtet-schedule-main-stage pa-1">{{event.english_event}}</div>
+                    <div flex="20" class="sdtet-schedule-date">{{event.start_time}}</div>
+                    <div flex="40" class="sdtet-schedule-cultural-village">{{event.viet_event}}</div>
+                </div>
 
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">B-Boy Battle</div>
-			  	<div flex="20" class="sdtet-schedule-date">6:00 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village"></div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">B-Boy Battle</div>
-			  	<div flex="20" class="sdtet-schedule-date">6:30 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village">Legendary Lion dance </div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">B-Boy Battle</div>
-			  	<div flex="20" class="sdtet-schedule-date">7:00 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village">Live Music & Traditional Singers</div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Van Kiep Lion Dance</div>
-			  	<div flex="20" class="sdtet-schedule-date">7:30 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village"></div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Step Up Dance</div>
-			  	<div flex="20" class="sdtet-schedule-date">8:00 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village"></div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">EDM Concert w/ Ha Bros</div>
-			  	<div flex="20" class="sdtet-schedule-date">9:00 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village"></div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Festival Closes</div>
-			  	<div flex="20" class="sdtet-schedule-date">10:00 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village">Festival Closes</div>
-				
-			    <!-- Saturday -->
-				<div class="sdtet-schedule-spacer"></div>
-			  	<div flex="40" class="sdtet-schedule-title sdtet-text-align-right">Main Stage</div>
-			  	<div flex="20" class="sdtet-schedule-date">SATURDAY 02.17.18</div>
-			  	<div flex="40" class="sdtet-schedule-title sdtet-text-align-left">Cultural Village</div>
-			  	<div class="sdtet-schedule-divider"></div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Festival Opens - Taiko Drums</div>
-			  	<div flex="20" class="sdtet-schedule-date">11:00 AM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village"></div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Opening Ceremony</div>
-			  	<div flex="20" class="sdtet-schedule-date">12:00 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village">Legendary Lion Dance</div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Firecrackers</div>
-			  	<div flex="20" class="sdtet-schedule-date">1:00 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village">Royal Procession (Quân Vương Hoàng Hậu)</div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Hoa Nghiêm Lion Dance & Traditional</div>
-			  	<div flex="20" class="sdtet-schedule-date">1:05 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village">Martial Arts</div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Black Leather Couch</div>
-			  	<div flex="20" class="sdtet-schedule-date">2:00 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village">Children’s Programs (Chương Trình Thiếu Nhi)</div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Southern Seas Lion Dance</div>
-			  	<div flex="20" class="sdtet-schedule-date">3:00 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village"></div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Mystique Band</div>
-			  	<div flex="20" class="sdtet-schedule-date">3:30 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village"></div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Mystique Band</div>
-			  	<div flex="20" class="sdtet-schedule-date">4:00 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village">Lion Dance </div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Traditional Dance</div>
-			  	<div flex="20" class="sdtet-schedule-date">4:30 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village">Live Cooking Demonstration with Executive Chef Ky Phan </div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Legendary Lion Dance</div>
-			  	<div flex="20" class="sdtet-schedule-date">5:00 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village"></div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Miss Vietnam Pageant <br/>w/ Thanh Hà & Lai Anh Tú</div>
-			  	<div flex="20" class="sdtet-schedule-date">5:30 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village">Phở Eating Contest</div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Miss Vietnam Pageant Winners</div>
-			  	<div flex="20" class="sdtet-schedule-date">9:30 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village"></div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Festival Closes</div>
-			  	<div flex="20" class="sdtet-schedule-date">10:00 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village">Festival Closes</div>
-
-			    <!-- Sunday -->
-			 	<div class="sdtet-schedule-spacer"></div>
-			  	<div flex="40" class="sdtet-schedule-title sdtet-text-align-right">Main Stage</div>
-			  	<div flex="20" class="sdtet-schedule-date">SUNDAY 02.18.18</div>
-			  	<div flex="40" class="sdtet-schedule-title sdtet-text-align-left">Cultural Village</div>
-			  	<div class="sdtet-schedule-divider"></div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Festival Opens - Taiko Drums</div>
-			  	<div flex="20" class="sdtet-schedule-date">11:00 AM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village">Festival Opens</div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Legendary Lion Dance</div>
-			  	<div flex="20" class="sdtet-schedule-date">12:00 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village">Tradition Wedding Procession (Đám Cưới Đầu Xuân)</div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Golden Voice</div>
-			  	<div flex="20" class="sdtet-schedule-date">1:00 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village">Kid’s Spot Light (Thi Bé Ngoan Bé Đẹp)</div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Tú Quyên & Andy Quách</div>
-			  	<div flex="20" class="sdtet-schedule-date">2:00 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village"></div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Golden Voice</div>
-			  	<div flex="20" class="sdtet-schedule-date">2:30 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village"></div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Golden Voice</div>
-			  	<div flex="20" class="sdtet-schedule-date">3:00 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village">Asian Story Theatre's "Foo Dog Fu"</div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Southern Seas Lion Dance</div>
-			  	<div flex="20" class="sdtet-schedule-date">3:30 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village">Dog Costume Contest</div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Tú Quyên & Andy Quách</div>
-			  	<div flex="20" class="sdtet-schedule-date">4:00 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village"></div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Golden Voice Final</div>
-			  	<div flex="20" class="sdtet-schedule-date">4:30 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village"></div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Tú Quyên & Andy Quách</div>
-			  	<div flex="20" class="sdtet-schedule-date">5:00 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village"></div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Mystique Band</div>
-			  	<div flex="20" class="sdtet-schedule-date">5:30 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village"></div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Van Kiep Lion Dance</div>
-			  	<div flex="20" class="sdtet-schedule-date">6:00 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village">Live Music & Traditional singers</div>
-
-			  	<div flex="40" class="sdtet-schedule-main-stage pa-1">Festival Closes</div>
-			  	<div flex="20" class="sdtet-schedule-date">10:00 PM</div>
-			  	<div flex="40" class="sdtet-schedule-cultural-village">Festival Closes</div>
-			  	<div class="sdtet-schedule-spacer"></div>
-			</div>
-		</div>
-	</div>
+                <div class="sdtet-schedule-spacer"></div>
+            </div>
+        </div>
+    </div>
 </div>
 </template>
+
+<script type="text/javascript">
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      friday: [],
+      saturday: [],
+      sunday: []
+    }
+  },
+  created () {
+    axios.get('https://cors.now.sh/http://admin.sdtet.com/php_file/get_festival_schedule.php')
+    .then(response => {
+      // JSON responses are automatically parsed.
+      var moment = require('moment')
+      for (var day in response.data) {
+        // console.log(day)
+        for (var item in response.data[day]) {
+          // console.log(item)
+          for (var property in response.data[day][item]) {
+            // console.log(property === 'start_time')
+            if (property === 'start_time') {
+              // console.log(response.data[day][item].start_time)
+              response.data[day][item].start_time = moment(response.data[day][item].start_time, 'HH:mm').format('h:mm A')
+            }
+          }
+        }
+      }
+      this.friday = response.data[Object.keys(response.data)[0]]
+      this.saturday = response.data[Object.keys(response.data)[1]]
+      this.sunday = response.data[Object.keys(response.data)[2]]
+      console.log(response.data)
+    })
+    .catch(e => {
+      console.log(e)
+    })
+  }
+}
+</script>
